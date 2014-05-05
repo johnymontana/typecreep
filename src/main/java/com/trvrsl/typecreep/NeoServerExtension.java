46e5362e5839becd822b40d108cfa87210e6d39b
johnymontana <lyonwj@gmail.com>
2014-05-04 18:15:58 -0600

    update classify cypher query

--- src/main/java/com/trvrsl/typecreep/NeoServerExtension.java
+++ src/main/java/com/trvrsl/typecreep/NeoServerExtension.java
@@ -45,11 +45,14 @@ public class NeoServerExtension
         params.put("sample", grams);
 
         String query =
+
                 "MATCH (a:Letter)-[k]-(b:Letter)-[*]->(u:User)\n" +
-                        "WHERE (a.char+b.char) IN {sample} \n" +
-                        "WITH (a.char+b.char) AS gram, a, b, u, k\n" +
-                        "WITH avg(a.duration) as avg_a, stdev(a.duration) as stdev_a, avg(b.duration) as avg_b, avg(k.duration) as avg_k, u, gram, count(k) as n, stdev(b.duration) as stdev_b, stdev(k.duration) as stdev_k\n" +
-                        "RETURN gram, collect({a: avg_k, stdev_a: stdev_a, b: avg_k, k: avg_k, user:u.id, n: n, stdev_b: stdev_b, stdev_k: stdev_k}) as obs";
+                "WHERE (a.char+b.char) IN {sample} \n" +
+                "WITH (a.char+b.char) AS gram, a, b, u, k\n" +
+                "WITH avg(a.duration) as avg_a, stdev(a.duration) as stdev_a, avg(b.duration) as avg_b, avg(k.duration) as avg_k, u, gram, count(k) as n, stdev(b.duration) as stdev_b, stdev(k.duration) as stdev_k\n" +
+                "WITH [avg_a, avg_k, avg_b] AS vector, avg_a, avg_b, avg_k, stdev_a, stdev_b, stdev_k, gram, n, u\n" +
+                "RETURN gram, collect({vector: vector, a: avg_k, stdev_a: stdev_a, b: avg_k, k: avg_k, user:u.id, n: n, stdev_b: stdev_b, stdev_k: stdev_k}) as obs";
+
 
         Map<String,List<Map<String, Object>>> resultsMap = new HashMap<String, List<Map<String, Object>>>();
 
