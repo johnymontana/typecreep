02326a405a7642608e852cb8233373a1e88c63b0
johnymontana <lyonwj@gmail.com>
2014-05-04 18:07:50 -0600

    fix sample/grams typo

--- src/main/java/com/trvrsl/typecreep/NeoServerExtension.java
+++ src/main/java/com/trvrsl/typecreep/NeoServerExtension.java
@@ -46,7 +46,7 @@ public class NeoServerExtension
 
         String query =
                 "MATCH (a:Letter)-[k]-(b:Letter)-[*]->(u:User)\n" +
-                        "WHERE (a.char+b.char) IN {grams} \n" +
+                        "WHERE (a.char+b.char) IN {sample} \n" +
                         "WITH (a.char+b.char) AS gram, a, b, u, k\n" +
                         "WITH avg(a.duration) as avg_a, stdev(a.duration) as stdev_a, avg(b.duration) as avg_b, avg(k.duration) as avg_k, u, gram, count(k) as n, stdev(b.duration) as stdev_b, stdev(k.duration) as stdev_k\n" +
                         "RETURN gram, collect({a: avg_k, stdev_a: stdev_a, b: avg_k, k: avg_k, user:u.id, n: n, stdev_b: stdev_b, stdev_k: stdev_k}) as obs";
