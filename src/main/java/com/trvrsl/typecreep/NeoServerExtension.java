68fa797d4381e46e453ee9ddbc1edc41fea77bef
johnymontana <lyonwj@gmail.com>
2014-05-10 13:42:58 -0600

    add single quote to define string

--- src/main/java/com/trvrsl/typecreep/NeoServerExtension.java
+++ src/main/java/com/trvrsl/typecreep/NeoServerExtension.java
@@ -59,9 +59,9 @@ public class NeoServerExtension
             String first = gram.substring(0,1);
             String second = gram.substring(1,2);
 
-            if (first.length() == 1 && second.length() == 1 && !first.equals(" ") && !second.equals(" ")) {
+            if (first.length() == 1 && second.length() == 1) { // && !first.equals(" ") && !second.equals(" ")) {
 
-                query += "WHERE (a.char =" + first + " AND b.char =" + second + ")";
+                query += "WHERE (a.char ='" + first + "' AND b.char ='" + second + "')";
 
                 if (gram_it.hasNext()) {
                     query += " OR \n";
